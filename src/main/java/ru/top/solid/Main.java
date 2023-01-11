d7c1831ef6c6d53fa48cc486911fed6562dbfcb1
vyatich <vik_ivy@mail.ru>
2023-01-11 20:39:54 +0300

    SOLID

---
+++ src/main/java/ru/top/solid/Main.java
@@ -0,0 +1,18 @@
+package ru.top.solid;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //SOLID
+        //S - single reponsibility principle (принцип единой ответственности)
+        //O - open-closed principle (принцип открытости-закрытости)
+        //L - Liskov substituion principle (принцип подстановки Барбары Лисков)
+        //I - interface segregation principle (принцип разделеняи интерфейсов)
+        //D- dependency inversion principle (принцип инверсии зависимостей)
+        //DRY - don't repeat yourself (не повторяйся)
+        //KISS - keep it stupid simple
+        //YAGNI - you are gonna need it (тебе это не нужно)
+
+
+    }
+}
